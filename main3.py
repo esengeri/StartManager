3455a63ee965b69dba63361b2964314ae9cf665b
Esengeldi <esenizamambekov@gmail.com>
2026-02-26 10:07:39 +0600

    Добовление календаря

---
+++ main3.py
@@ -0,0 +1,6 @@
+def chow_calendar():
+    print("\n--- Календарь SmartManager---")
+    print("Сегодняшний дата: 2026-год, 26-февраля")
+    print("Ближайшие событие: Сдать лабараторную работу")
+if __name__ == "__main__":
+    chow_calendar()
